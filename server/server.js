72a8bcd988d53d09d58b7ff08d69581c7ff58d97
lochan yoganathan <lochan.yoganathan@gmail.com>
2018-08-27 01:00:36 +0400

    Added GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express=require('express');
 var bodyParser=require('body-parser');
 
+const {ObjectID}=require('mongodb');
 var {mongoose}=require('./db/mongoose');
 var{Todo}=require('./models/todos');
 var{User}=require('./models/user');
@@ -30,11 +31,27 @@ Todo.find().then((todos)=>{res.send({todos});},
 (e)=>{res.status(400).send(e);});
 });
 
+//getting an individual resource
+app.get('/todos/:id',(req,res)=>{
+var id = req.params.id;
+if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+    
+ }
+//just using id to find
+Todo.findById(id).then((todo)=>
+{
+    if(!todo){
+        return res.status(404).send();
+    }
+    res.send({todo});
+}).catch((e)=>{res.status(400).send(e);});
+});
 
 app.listen(3000,()=>{
     console.log('Started on port 3000');
 });
-
+ 
 module.exports={app};
 
 
