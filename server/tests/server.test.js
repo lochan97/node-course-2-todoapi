a73281fb20937f766978d1476be2d35479631b95
lochan yoganathan <lochan.yoganathan@gmail.com>
2018-08-27 01:23:18 +0400

    added describe test for get by id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,13 +1,16 @@
 const expect=require('expect');
 const request=require('supertest');
+const {ObjectID}=require('mongodb');
 
 const {app}=require('./../server');
 const {Todo}=require('./../models/todos');
 
 //making an array of dummy todos to test the get
 const todos =[{
+    _id:new ObjectID(),
     text:'First test todo'
 },{
+    _id:new ObjectID(),
     text:'Second test todo'
 }]
 
@@ -82,3 +85,26 @@ describe('GET /todos',()=>{
         }).end(done);
     })
 })
+
+describe('GET /todos/:id',()=>{
+  it("should return todo doc",(done)=>{
+    request(app).get(`/todos/${todos[0]._id.toHexString()}`)
+.expect(200)
+.expect((res)=>{
+    expect(res.body.todo.text).toBe(todos[0].text);
+}) .end(done); 
+})  ;
+
+it("should return 404 if todo not found",(done)=>{
+   var nid= new ObjectID;
+    request(app).get(`/todos/${nid.toHexString()}`)
+.expect(404)
+ .end(done); 
+})  ;
+it("should return 404 for non object IDs",(done)=>{
+    var nid= new ObjectID;
+     request(app).get(`/todos/${123}`)
+ .expect(404)
+  .end(done); 
+ });
+});
